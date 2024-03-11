c03adabdd8fe7167277e8f3facccf087d36e16d0
Mario Lopez <114566160+B4bolFP@users.noreply.github.com>
2024-03-11 11:43:31 +0100

    asd

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement")]
-    public float moveSpeed;
+    [Header("Controles")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Movimiento")]
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
+
 
     public float groundDrag;
 
-    [Header("Ground Check")]
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+
+    [Header("Comprobacion suelo")]
     public float playerHeight;
-    public LayerMask Default;
+    public LayerMask queEsSuelo;
     bool grounded;
 
     public Transform orientation;
@@ -21,20 +34,31 @@ public class PlayerMovement : MonoBehaviour
     float verticalInput;
 
     Vector3 moveDirection;
-
     Rigidbody rb;
+
+    public MovementState state;
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        air
+    }
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        resetJump();
     }
 
     private void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Default);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, queEsSuelo);
+
         MyInput();
         SpeedControl();
+        StateHandler();
 
         if (grounded)
             rb.drag = groundDrag;
@@ -51,6 +75,36 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            jump();
+
+            Invoke(nameof(resetJump), jumpCooldown);
+        }
+    }
+
+    public void StateHandler()
+    {
+        // Corriendo
+        if(grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+
+        // Andando
+        } else if(grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+
+        // En el aire
+        } else
+        {
+            state = MovementState.air;
+        }
     }
 
     private void MovePlayer()
@@ -58,7 +112,13 @@ public class PlayerMovement : MonoBehaviour
         // Calculo de movimiento
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        if(grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        } else if(!grounded) {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+
     }
 
     private void SpeedControl()
@@ -71,5 +131,17 @@ public class PlayerMovement : MonoBehaviour
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
+    }
+
+    private void jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void resetJump()
+    {
+        readyToJump = true;
     }
 }
